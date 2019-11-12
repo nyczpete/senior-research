a5b2f58c770d324efc0d04a073322bd19b1e5097
Tan Dung Luong <47040093+nyczpete@users.noreply.github.com>
2019-11-12 17:36:36 -0500

    Add files via upload

--- application.py
+++ application.py
@@ -1,20 +1,36 @@
 # 
 # Senior Research Project
 # Tan Dung Luong
-# Suntae Park
+# Suntae Parks
 #
 
 from midi import *
 from music import * 
 
+# create midi input
 midiIn = MidiIn()     
+
+# create display
+display = Display("MIDI App - Senior Research", 1024, 768, 0, 0, Color.cyan)
+
+# coordinates
+x = 0
+y = 0
 
 def printNote(eventType, channel, data1, data2):
    # eventType: 144 = start, 128 = stop
    # channel: only one channel for the moment, channel 0
    # data 1 = pitch number
    # data 2 = volume level
+   global x
+   global y
    print "Pitch = ", MIDI_PITCHES[data1]
+   note = TextField(MIDI_PITCHES[data1])
+   if(x >= 900):
+      x = 0
+      y += 40
+   display.add(note, x, y)
+   x += 40
    
 # TODO: map pitches to images on staff lines for GUI
 
